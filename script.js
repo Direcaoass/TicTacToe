6fce9d314d63e901463fa0d8eac37f5a599f1106
Vitor <direcaoass@gmail.com>
2023-01-26 10:55:00 -0300

    refactored using class pattern

--- script.js
+++ script.js
@@ -1,55 +1,90 @@
+class TicTacToe {
+    constructor() {
+        this.X = "X";
+        this.O = "O";
+        this.currentPlayer = this.X;
+        this.matchCount = 0;
+        this.gameOver = false;
+        this.winCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        this.messageDisplay = document.querySelector(".msgDisplay");
+        this.restartButton = document.querySelector(".restart");
+        this.board = document.querySelector(".boardGrid");
+        this.init();
+    }
 
-let moveFlag = false;
-let matchCount = 0;
+    init() {
+        this.createBoard();
+        this.restartButton.addEventListener("click", () => this.restartGame());
+        this.messageDisplay.innerText = `${this.currentPlayer} player turn`;
+    }
 
-(function createGameBoard() {
-    const boardGrid = document.querySelector('.boardGrid');
-    for (let i = 1; i < 10; i++) {
-        const squareDiv = document.createElement('div');
-        squareDiv.classList.add('square')
-        boardGrid.appendChild(squareDiv);
-        squareDiv.onclick = () => startGame(squareDiv)
+    createBoard() {
+        for (let i = 0; i < 9; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("click", (event) => this.handleMove(event));
+            this.board.appendChild(square);
+        }
+    }
+
+    handleMove(event) {
+        if (!this.gameOver) {
+
+            const square = event.target;
+            if (!square.innerText) {
+                square.innerText = this.currentPlayer;
+                this.matchCount++;
+                this.checkForWin();
+                this.checkForDraw();
+                this.switchPlayer();
+            }
+            else this.messageDisplay.innerText = 'Invalid Move'
+        }
+    }
+
+    checkForWin() {
+        for (let i = 0; i < this.winCombinations.length; i++) {
+            const [a, b, c] = this.winCombinations[i];
+            if (this.board.children[a].innerText === this.currentPlayer && this.board.children[b].innerText === this.currentPlayer && this.board.children[c].innerText === this.currentPlayer) {
+                this.messageDisplay.innerText = `Game OVER >> Player ${this.currentPlayer} wins!`;
+                this.gameOver = true;
+                return;
+            }
+        }
+    }
+
+    checkForDraw() {
+        if (this.matchCount === 9) {
+            this.messageDisplay.innerText = " Game OVER >> It's a draw!";
+
+        }
+    }
+
+    switchPlayer() {
+        if (!this.gameOver) {
+            this.currentPlayer = this.currentPlayer === this.X ? this.O : this.X;
+            this.messageDisplay.innerText = `${this.currentPlayer} player turn`;
+        }
+    }
+
+    restartGame() {
+        this.matchCount = 0;
+        this.gameOver = false;
+        this.currentPlayer = this.X;
+        this.messageDisplay.innerText = "";
+        for (let i = 0; i < this.board.children.length; i++) {
+            this.board.children[i].innerText = "";
+        }
     }
 }
-)();
 
-
-
-// function getResult() {
-//     const boardArray = document.querySelectorAll(".square")
-
-//     if (boardArray[0].innerText === boardArray[1].innerText && boardArray[0].innerText === boardArray[2].innerText)
-//         return alert(`${boardArray[0].innerText} player wins`)
-
-//     else if (boardArray[3].innerText === boardArray[4].innerText && boardArray[3].innerText === boardArray[5].innerText)
-//         return alert(`${boardArray[3].innerText} player wins`)
-
-//     else if (boardArray[6].innerText === boardArray[7].innerText && boardArray[6].innerText === boardArray[8].innerText)
-//         return alert(`${boardArray[6].innerText} player wins`)
-
-// }
-
-
-
-
-
-function startGame(squareDiv) {
-    let move = getMove();
-    if  (matchCount > 8) return alert('End Game?')
-    else
-        (squareDiv.innerText) ? alert('Invalid move.') : squareDiv.innerText = move;
-    // getResult();
-    const playerTurn = document.querySelector('.turn');
-    playerTurn.innerText = moveFlag ? 'O player turn!' : 'X player turn!';
-    matchCount++;
-    console.log(matchCount);
-
-
-}
-
-function getMove() {
-    moveFlag = !moveFlag;
-    let moveValue = moveFlag ? 'X' : 'O'
-    return moveValue
-}
-
+new TicTacToe();
